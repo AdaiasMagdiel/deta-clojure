8bfbca17c3345687a33bb0467b77c2ff937a6e63
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-11 18:30:25 -0300

    chore(project): Update project description and add repository details
    Update the project description in the project.clj file to make it more
    descriptive and understandable. Also, added repository details with the
    necessary configurations for easy access to dependencies from the specified
    repository.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.adaiasmagdiel/deta "0.0.1"
-  :description "A library in Clojure to use and manage Deta Base."
+  :description "A Clojure library designed to simplify interactions with the Deta database. It provides a straightforward way to initialize a database connection and perform operations."
   :url "http://github.com/AdaiasMagdiel/deta-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/license/mit"}
@@ -7,4 +7,7 @@
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.5.0"]
                  [clj-http "3.12.3"]]
-  :repl-options {:init-ns deta})
+  :repl-options {:init-ns deta}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg
+                              :sign-releases true}]])
