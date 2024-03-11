63ce96f098a33bd5dc3ee98467a0c03d8f4db375
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-11 16:31:00 -0300

    feat(project): Initialize project.clj with basic configuration
    This commit adds the project.clj file with initial configuration:
    - added project information like name, version, and description
    - included license details
    - defined project dependencies
    - set up REPL options for deta.core namespace.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject deta "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url "https://opensource.org/license/mit"}
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.5.0"]
+                 [clj-http "3.12.3"]]
+  :repl-options {:init-ns deta.core})
