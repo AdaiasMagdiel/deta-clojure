d4dc3276ed61ebd59755470f1a3f3119895bbf0c
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-03-25 18:54:55 -0300

    chore(project): Update project.clj description and add lein-cloverage plugin
    Update project.clj description to mention interactions with Deta Base and Deta
    Drive services, improving clarity and accuracy of the library purpose.
    
    Add lein-cloverage plugin to enable code coverage reports for better
    understanding of test coverage within the project.
    
    These changes enhance the project documentation and development processes for
    better maintenance and understanding of the project functionalities.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.adaiasmagdiel/deta "0.0.5"
-  :description "A Clojure library designed to simplify interactions with the Deta database. It provides a straightforward way to initialize a database connection and perform operations."
+  :description "A Clojure library designed to simplify interactions with the Deta Base and Deta Drive services. It provides a straightforward way to initialize a service connection and perform operations."
   :url "http://github.com/AdaiasMagdiel/deta-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/license/mit"}
@@ -7,6 +7,7 @@
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.5.0"]
                  [clj-http "3.12.3"]]
+  :plugins [[lein-cloverage "1.2.2"]]
   :repl-options {:init-ns deta}
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg
